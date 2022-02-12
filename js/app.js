8a5f79df955341ebf0f428bf0ff797639f34afd2
Paolaguerralibrero <mpaolamcra@hotmail.com>
2022-02-12 22:46:32 +0000

    added more options

--- js/app.js
+++ js/app.js
@@ -8,10 +8,50 @@ form.addEventListener('submit', handleFormSubmit);
 
 const handleFormSubmit = function(event){
     event.preventDefault();
-    const name = event.target.names.value;
-    console.log(event.target.names.value);
-    const nameElement = document.createElement('p')
-    nameElement.textContent = name
-    const namesList = document.querySelector('#names-list');
-    namesList.appendChild(nameElement);
+
+    const newPlantItem = createPlantItem(event.target);
+    const plantList = document.querySelector('#plant-list');
+    plantList.appendChild(newPlantItem);
+
+    event.target.reset();
+
 }
+
+const createPlantItem = function (form) {
+    const newPlantItem = document.createElement('li');
+    newPlantItem.classList.add('plant-item');
+
+    const name = document.createElement('h2');
+    name.textContent = form.names.value;
+    newPlantItem.appendChild(name)
+
+    const species = document.createElement('h3');
+    species.textContent = form.species.value;
+    newPlantItem.appendChild(species)
+
+    const origin = document.createElement('p');
+    origin.textContent = form.origin.value;
+    newPlantItem.appendChild(origin);
+    
+    return newPlantItem;
+}
+
+
+
+
+
+
+
+
+
+
+
+// const handleFormSubmit = function(event){
+//     event.preventDefault();
+//     const name = event.target.names.value;
+//     console.log(event.target.names.value);
+//     const nameElement = document.createElement('p')
+//     nameElement.textContent = name
+//     const namesList = document.querySelector('#names-list');
+//     namesList.appendChild(nameElement);
+// }
